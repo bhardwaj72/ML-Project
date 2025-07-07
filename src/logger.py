e17da89772f3b5bf0a9c5290bb7ce3998515135b
Vivek Bhardwaj <Vivekbhardwaj12.278@gmail.com>
2025-07-07 13:51:57 +0530

    took 2 Hrs to find the bug

--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,19 @@
 import logging
 import os
 from datetime import datetime
-
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y-%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path,exist_ok = True)
+import sys
 
 
-LOG_FilE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FilE_PATH,
-    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
-    level = logging.INFO,
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
 
 )
-
